d06d804dcad4351a99cf572fcee7f45ecc0ca0cf
Milton-Agudelo <Milton_Agudelo@yahoo.com>
2024-05-02 10:17:51 -0500

    Client API implementation.

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -8,11 +8,13 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import django
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
-import django
+
 django.setup()
 
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
